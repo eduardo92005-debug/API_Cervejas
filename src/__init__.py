0b505bd1dfc320266163299cc2fa407515b6349a
carlos <carlos@pop-os.localdomain>
2022-12-12 02:37:50 -0300

    feat: tabela populada, adicionado schemas e correcoes gerais

--- src/__init__.py
+++ src/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config.from_object('config')
 api = Api(app)
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 migrate = Migrate(app, db)
 
 from .views import beer_views
