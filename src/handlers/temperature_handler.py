f98805b9ed1cd51e4c1e6b541dc44ecaa07a013c
carlos <carlos@pop-os.localdomain>
2022-12-12 23:41:37 -0300

    feat: adicionada funcionalidade de pegar o input mais proximo das temperaturas

--- src/handlers/temperature_handler.py
+++ src/handlers/temperature_handler.py
@@ -1,4 +1,5 @@
 from sqlalchemy import text
+from ..utils import dict_utils
 from src import db
 
 def get_average_table_temperature():
@@ -6,6 +7,14 @@ def get_average_table_temperature():
     response = db.session.execute(sql_text)
     return response
 
-def get_nearest_value_from_temperature(input):
+def get_nearest_value_from_temperature(temperature_input):
     average_table = get_average_table_temperature()
-    print(average_table)
+    dict_distance = {}
+    for i in average_table:
+        distance = abs(temperature_input - i.average)
+        dict_distance[i.id_best_beer_temperature] = distance
+    value_min_distance = min(dict_distance.values())
+    #Get ID from beers that has the nearest value to the input
+    list_key_min_distance = dict_utils.get_key_dict(dict_distance, value_min_distance)
+    return list_key_min_distance
+
--- src/repositories/beer_repository.py
+++ src/repositories/beer_repository.py
@@ -1,5 +1,6 @@
 from ..models import beer_model
 from sqlalchemy import text
+from sqlalchemy.sql import or_
 from src import db
 from ..handlers import temperature_handler
 
@@ -11,8 +12,15 @@ def list_beer_by_id(id):
     beer = beer_model.Beers.query.filter_by(id_beer=id).first()
     return beer
 
+#def list_beer_by_temperature(temperature):
+#    beer = beer_model.BestTemperatureBeers.query.filter_by(max_best_temperature=temperature).first()
+#    return beer
+
 def list_beer_by_temperature(temperature):
-    beer = beer_model.BestTemperatureBeers.query.filter_by(max_best_temperature=temperature).first()
+    list_key_nearest_id_beer = temperature_handler.get_nearest_value_from_temperature(temperature)
+    first_id = list_key_nearest_id_beer[0]
+    beer = db.session.query(beer_model.BestTemperatureBeers).filter(or_(beer_model.BestTemperatureBeers.id_best_beer_temperature == v for v in list_key_nearest_id_beer))
+    beer = beer.order_by(beer_model.BestTemperatureBeers.style_beer.asc()).all()
     return beer
 
 def list_all_beer_and_average():
