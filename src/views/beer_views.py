7593d2b9638366f3291c85eb11a0686f90fa7b17
carlos <carlos@pop-os.localdomain>
2022-12-12 21:34:11 -0300

    fead and fixes: adicionado pasta de handlers e correcao em algumas features, rota para temperaturas criadas

--- src/views/beer_views.py
+++ src/views/beer_views.py
@@ -1,20 +1,20 @@
 from flask_restful import Resource
 from flask import jsonify, make_response
 from src import api
-from ..services import beer_service
+from ..repositories import beer_repository
 from ..schemas import beer_schema
 
 
 class BeerList(Resource):
     def get(self):
-        beers = beer_service.list_beers()
+        beers = beer_repository.list_beers()
         bs = beer_schema.BeerSchema(many=True)
         return make_response(bs.jsonify(beers), 200)
 
 
 class BeerDetail(Resource):
     def get(self, id):
-        beer = beer_service.list_beer_by_id(id)
+        beer = beer_repository.list_beer_by_id(id)
         if beer is None:
             return make_response(jsonify("Not found"), 404)
         else:
@@ -28,9 +28,16 @@ class BeerDetail(Resource):
         pass
 
 
+
+class BeerTemperatureList(Resource):
+    def get(self):
+        beers = beer_repository.list_all_beer_and_average()
+        bs = beer_schema.BestBeerTemperatureSchema(many=True)
+        return make_response(bs.jsonify(beers), 200)
+
 class BeerTemperatureDetail(Resource):
     def get(self, temperature):
-        beer = beer_service.list_beer_by_temperature(temperature)
+        beer = beer_repository.list_beer_by_temperature(temperature)
         if beer is None:
             return make_response(jsonify("Not found"), 404)
         else:
@@ -41,3 +48,4 @@ class BeerTemperatureDetail(Resource):
 api.add_resource(BeerList, '/beers')
 api.add_resource(BeerDetail, '/beers/<int:id>')
 api.add_resource(BeerTemperatureDetail, '/beers/temperature/<int:temperature>')
+api.add_resource(BeerTemperatureList, '/beers/temperature')
