864b55559a1056505e5ecde1bd211f96df249927
carlos <carlos@pop-os.localdomain>
2022-12-14 01:04:40 -0300

    feat:adicionado swagger no flask

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,6 +11,7 @@ api = Api(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
+swagger = Swagger(app)
 
 from .views import beer_views
 from .models import beer_model
--- src/views/beer_views.py
+++ src/views/beer_views.py
@@ -8,6 +8,33 @@ from ..schemas import beer_schema
 
 class BeerList(Resource):
     def get(self):
+        """
+            Returns all beers in the database
+            ---
+            tags:
+                - beers
+            responses:
+                200:
+                    description: The list of beers
+                    schema:
+                        id: beers
+                        properties:
+                            id_beer:
+                                type: integer
+                                description: The beer id
+                            style_beer:
+                                type: string
+                                description: The beer style
+                            id_best_beer_temperature:
+                                type: integer
+                                description: The beer best temperature id
+                            created_at:
+                                type: string
+                                description: The beer creation date
+                            updated_at:
+                                type: string
+                                description: The beer update date
+        """
         beers = beer_repository.list_beers()
         bs = beer_schema.BeerSchema(many=True)
         return make_response(bs.jsonify(beers), 200)
@@ -15,6 +42,41 @@ class BeerList(Resource):
 
 class BeerDetail(Resource):
     def get(self, id):
+        """
+            Returns a beer by id
+            ---
+            tags:
+                - beers
+            parameters:
+                - in: path
+                  name: id
+                  type: integer
+                  required: true
+                  description: The beer id
+            responses:
+                200:
+                    description: The beer
+                    schema:
+                        id: beer
+                        properties:
+                            id_beer:
+                                type: integer
+                                description: The beer id
+                            style_beer:
+                                type: string
+                                description: The beer style
+                            id_best_beer_temperature:
+                                type: integer
+                                description: The beer best temperature id
+                            created_at:
+                                type: string
+                                description: The beer creation date
+                            updated_at:
+                                type: string
+                                description: The beer update date
+                404:
+                    description: The beer was not found
+        """
         beer = beer_repository.list_beer_by_id(id)
         if beer is None:
             return make_response(jsonify("Not found"), 404)
@@ -32,12 +94,88 @@ class BeerDetail(Resource):
 
 class BeerTemperatureList(Resource):
     def get(self):
+        """
+            Returns all beers with average temperature
+            ---
+            tags:
+                - beers
+            responses:
+                200:
+                    description: The list of beers
+                    schema:
+                        id: beers
+                        properties:
+                            id_best_beer_temperature:
+                                type: integer
+                                description: The beer best temperature id
+                            style_beer:
+                                type: string
+                                description: The beer style
+                            min_best_temperature:
+                                type: integer
+                                description: The beer min best temperature
+                            max_best_temperature:
+                                type: integer
+                                description: The beer max best temperature
+                            average:
+                                type: integer
+                                description: The beer average temperature
+                            created_at:
+                                type: string
+                                description: The beer creation date
+                            updated_at:
+                                type: string
+                                description: The beer update date
+        """
         beers = beer_repository.list_all_beer_and_average()
         bs = beer_schema.BestBeerTemperatureSchemaAll(many=True)
         return make_response(bs.jsonify(beers), 200)
 
 class BeerTemperatureDetail(Resource):
     def get(self, temperature):
+        """
+            Returns a beer by temperature and a playlist related with them
+            ---
+            tags:
+                - beers
+            parameters:
+                - in: path
+                  name: temperature
+                  type: integer
+                  required: true
+                  description: The beer temperature
+            responses:
+                200:
+                    description: The beer and a playlist
+                    schema:
+                        id: playlist
+                        properties:
+                            input_beer:
+                                type: string
+                                description: the input beer in the path
+                            name:
+                                type: string
+                                description: the playlist name
+                            tracks:
+                                type: array
+                                description: the playlist tracks
+                                artist:
+                                    type: string
+                                    description: the track artist
+                                name:
+                                    type: string
+                                    description: the track name
+                                link:
+                                    type: string
+                                    description: the track link
+                            style_beer:
+                                type: string
+                                description: The beer style
+                404:
+                    description: The beer was not found
+                Error:
+                    description: The spotify service is not available, check bearer token                  
+        """
         beer = beer_repository.list_beer_by_temperature(temperature)
         if beer is None:
             return make_response(jsonify("Not found"), 404)
@@ -47,9 +185,9 @@ class BeerTemperatureDetail(Resource):
             json_list = []
             for beer_element in bts_json.json:
                 request_response = requests_handlers.request_spotify_service(beer_element["style_beer"])
-                related = beer_element | request_response
+                related = request_response | beer_element
                 json_list.append(related)
-            return json_list
+            return make_response(json_list,200)
 
 
 api.add_resource(BeerList, '/beers')
