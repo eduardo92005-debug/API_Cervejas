28c3792f986993ac7a4e836397b715dcd8805eac
carlos <carlos@pop-os.localdomain>
2022-12-15 01:10:13 -0300

    feat: terminado crud do beer e beer temperature e documentado

--- src/services/beer_temperature_service.py
+++ src/services/beer_temperature_service.py
@@ -8,10 +8,11 @@ def insert_beer_temperature(beer):
     db.session.commit()
     return beer_bd
 
-def update_beer(beer):
-    beer_bd = beer_model.Beers.query.filter_by(id_beer=beer.id_beer).first()
+def update_beer_temperature(beer):
+    beer_bd = beer_model.BestTemperatureBeers.query.filter_by(id_best_beer_temperature=beer.id_best_beer_temperature).first()
     beer_bd.style_beer = beer.style_beer
-    beer_bd.id_best_beer_temperature = beer.id_best_beer_temperature
+    beer_bd.min_best_temperature = beer.min_best_temperature
+    beer_bd.max_best_temperature = beer.max_best_temperature
     beer_bd.updated_at = datetime.utcnow()
     db.session.commit()
     return beer_bd
--- src/views/beer_views.py
+++ src/views/beer_views.py
@@ -14,7 +14,7 @@ class BeerList(Resource):
             Returns all beers in the database
             ---
             tags:
-                - beers
+                - Beers
             responses:
                 200:
                     description: The list of beers
@@ -46,7 +46,7 @@ class BeerList(Resource):
             Insert a new beer in the database
             ---
             tags:
-                - beers
+                - Beers
             parameters:
                 - in: body
                   name: beers
@@ -103,11 +103,13 @@ class BeerList(Resource):
             try:
                 style_beer = request.json["style_beer"]
                 id_best_beer_temperature = request.json["id_best_beer_temperature"]
-                check_id_beer_temp = beer_repository.list_beer_temperature_by_id(id_best_beer_temperature)
+                check_id_beer_temp = beer_repository.list_beer_temperature_by_id(
+                    id_best_beer_temperature)
                 if check_id_beer_temp is None:
                     return make_response("The id_best_beer_temperature passed does not exist in the database", 404)
                 else:
-                    new_beer = beer_entity.Beer(style_beer, id_best_beer_temperature)
+                    new_beer = beer_entity.Beer(
+                        style_beer, id_best_beer_temperature)
                     result = beer_service.insert_beer(new_beer)
                     return make_response(bs.jsonify(result), 201)
             except Exception as e:
@@ -120,7 +122,7 @@ class BeerTemperatureList(Resource):
             Returns all beers with average temperature
             ---
             tags:
-                - beers
+                - Beers temperature
             responses:
                 200:
                     description: The list of beers
@@ -152,7 +154,70 @@ class BeerTemperatureList(Resource):
         beers = beer_repository.list_all_beer_and_average()
         bs = beer_schema.BestBeerTemperatureSchemaAll(many=True)
         return make_response(bs.jsonify(beers), 200)
+
     def post(self):
+        """
+            Insert a new beer temperature in the database
+            ---
+            tags:
+                - Beers temperature
+            parameters:
+                - in: body
+                  name: beers
+                  description: The beer temperature to insert
+                  schema:
+                    id: beer
+                    properties:
+                        style_beer:
+                            type: string
+                            description: The beer style
+                        min_best_temperature:
+                            type: integer
+                            description: The beer min best temperature
+                        max_best_temperature:
+                            type: integer
+                            description: The beer max best temperature 
+            responses:
+                201:
+                    description: The beer temperature was inserted
+                    schema:
+                        id: beer
+                        properties:
+                            id_best_beer_temperature:
+                                type: integer
+                                description: The beer best temperature id
+                            style_beer:
+                                type: string
+                                description: The beer style
+                            min_best_temperature:
+                                type: integer
+                                description: The beer min best temperature
+                            max_best_temperature:
+                                type: integer
+                                description: The beer max best temperature
+                            created_at:
+                                type: string
+                                description: The beer creation date
+                            updated_at: 
+                                type: string
+                                description: The beer update date
+                400:
+                    description: The beer temperature was not inserted
+                    schema:
+                        id: error
+                        properties:
+                            style_beer:
+                                type: string
+                                description: The beer style
+                            min_best_temperature:
+                                type: integer
+                                description: The beer min best temperature
+                            max_best_temperature:
+                                type: integer
+                                description: The beer max best temperature
+                500:
+                    description: The beer temperature was not inserted, internal server error
+        """
         bs = beer_schema.BestBeerTemperatureSchemaAll()
         validate = bs.validate(request.json)
         if validate:
@@ -162,8 +227,10 @@ class BeerTemperatureList(Resource):
                 style_beer = request.json["style_beer"]
                 min_best_temperature = request.json["min_best_temperature"]
                 max_best_temperature = request.json["max_best_temperature"]
-                new_beer = beer_entity.BeerTemperatureEntity(style_beer, min_best_temperature, max_best_temperature)
-                result = beer_temperature_service.insert_beer_temperature(new_beer)
+                new_beer = beer_entity.BeerTemperatureEntity(
+                    style_beer, min_best_temperature, max_best_temperature)
+                result = beer_temperature_service.insert_beer_temperature(
+                    new_beer)
                 return make_response(bs.jsonify(result), 201)
             except Exception as e:
                 return make_response("Check if the data was passed correctly or a bad code: " + str(e), 500)
@@ -177,7 +244,7 @@ class BeerDetail(Resource):
             Returns a beer by id
             ---
             tags:
-                - beers
+                - Beers
             parameters:
                 - in: path
                   name: id
@@ -220,7 +287,7 @@ class BeerDetail(Resource):
             Update a beer by id
             ---
             tags:
-                - beers
+                - Beers
             parameters:
                 - in: path
                   name: id
@@ -308,7 +375,7 @@ class BeerDetail(Resource):
             Delete a beer by id
             ---
             tags:
-                - beers
+                - Beers
             parameters:
                 - in: path
                   name: id
@@ -330,14 +397,168 @@ class BeerDetail(Resource):
 
 
 class BeerTemperatureDetail(Resource):
-    def get(self, temperature):
+    def get(self, id):
+        """
+            Returns a beer temperature by id
+            ---
+            tags:
+                - Beers temperature
+            parameters:
+                - in: path
+                  name: id
+                  type: integer
+                  required: true
+                  description: The beer temperature id
+            responses:
+                200:
+                    description: The beer temperature
+                    schema:
+                        id: beer_temperature
+                        properties:
+                            id_best_beer_temperature:
+                                type: integer
+                                description: The beer temperature id
+                            min_best_temperature:
+                                type: integer
+                                description: The beer min temperature
+                            max_best_temperature:
+                                type: integer
+                                description: The beer max temperature
+                            created_at:
+                                type: string
+                                description: The beer temperature creation date
+                            updated_at:
+                                type: string
+                                description: The beer temperature update date
+                404:
+                    description: The beer temperature id was not found
+
+        """
+        beer = beer_repository.list_beer_temperature_by_id(id)
+        if beer is None:
+            return make_response(jsonify("Beer Temperature ID not found"), 404)
+        else:
+            bs = beer_schema.BestBeerTemperatureSchemaAll()
+            beer.average = (beer.max_best_temperature + beer.min_best_temperature) / 2
+            return make_response(bs.jsonify(beer), 200)
+    
+    def put(self,id):
+        """
+            Update a beer temperature by id
+            ---
+            tags:
+                - Beers temperature
+            parameters:
+                - in: path
+                  name: id
+                  type: integer
+                  required: true
+                  description: The beer temperature id
+                - in: body
+                  name: beer_temperature
+                  description: The beer temperature to update
+                  schema:
+                        id: beer_temperature
+                        properties:
+                            style_beer:
+                                type: string
+                                description: The beer style
+                            max_best_temperature:
+                                type: integer
+                                description: The beer max temperature
+                            min_best_temperature:
+                                type: integer
+                                description: The beer min temperature
+            responses:
+                200:
+                    description: The beer temperature was updated
+                    schema:
+                        id: beer_temperature
+                        properties:
+                            id_best_beer_temperature:
+                                type: integer
+                                description: The beer temperature id
+                            style_beer:
+                                type: string
+                                description: The beer style
+                            max_best_temperature:
+                                type: integer
+                                description: The beer max temperature
+                            min_best_temperature:
+                                type: integer
+                                description: The beer min temperature
+                            created_at:
+                                type: string
+                                description: The beer temperature creation date
+                            updated_at:
+                                type: string
+                                description: The beer temperature update date
+                400:
+                    description: The beer temperature was not updated, bad request
+                404:
+                    description: The beer temperature id was not found
+                500:
+                    description: The beer temperature was not updated, internal server error
+        """
+        id_best_beer_temperature = id
+        beer = beer_repository.list_beer_temperature_by_id(id_best_beer_temperature)
+        if beer is None:
+            return make_response(jsonify("Best Beer Temperature ID not found"), 404)
+        else:
+            bs = beer_schema.BestBeerTemperatureSchemaAll()
+            validate = bs.validate(request.json)
+            if validate:
+                return make_response(jsonify(validate), 400)
+            else:
+                try:
+                    style_beer = request.json["style_beer"]
+                    max_best_temperature = request.json["max_best_temperature"]
+                    min_best_temperature = request.json["min_best_temperature"]
+                    beer.id_beer = id_best_beer_temperature
+                    beer.style_beer = style_beer
+                    beer.max_best_temperature = max_best_temperature
+                    beer.min_best_temperature = min_best_temperature
+                    result = beer_temperature_service.update_beer_temperature(beer)
+                    return make_response(bs.jsonify(result), 200)
+                except Exception as e:
+                    return make_response("Check if the data was passed correctly or a bad code: " + str(e), 500)
+
+    def delete(self,id):
+        """
+            Delete a beer temperature by id
+            ---
+            tags:
+                - Beers temperature
+            parameters:
+                - in: path
+                  name: id
+                  type: integer
+                  required: true
+                  description: The beer temperature id
+            responses:
+                204:
+                    description: The beer temperature was deleted, no content to show
+                404:
+                    description: The beer temperature id was not found
+        """
+        id_best_beer_temperature = id
+        beer = beer_repository.list_beer_temperature_by_id(id_best_beer_temperature)
+        if beer is None:
+            return make_response(jsonify("Best Beer Temperature ID not found"), 404)
+        else:
+            result = beer_temperature_service.delete_beer_temperature(beer)
+            return make_response('', 204)
+
+
+class BeerTemperaturePlaylistList(Resource):
+    def get(self):
         """
             Returns a beer by temperature and a playlist related with them
             ---
             tags:
-                - beers
+                - Beers temperature
             parameters:
-                - in: path
+                - in: body
                   name: temperature
                   type: integer
                   required: true
@@ -348,9 +569,6 @@ class BeerTemperatureDetail(Resource):
                     schema:
                         id: playlist
                         properties:
-                            input_beer:
-                                type: string
-                                description: the input beer in the path
                             name:
                                 type: string
                                 description: the playlist name
@@ -374,6 +592,7 @@ class BeerTemperatureDetail(Resource):
                 Error:
                     description: The spotify service is not available, check bearer token                  
         """
+        temperature = request.json["temperature"]
         beer = beer_repository.list_beer_by_temperature(temperature)
         if beer is None:
             return make_response(jsonify("Not found"), 404)
@@ -387,44 +606,12 @@ class BeerTemperatureDetail(Resource):
                 json_list.append(related)
             return make_response(json_list,200)
 
-    def put(self,id):
-        beer = beer_repository.list_beer_temperature_by_id(id)
-        if beer is None:
-            return make_response(jsonify("Beer ID not found"), 404)
-        else:
-            bs = beer_schema.BestBeerTemperatureSchemaAll()
-            validate = bs.validate(request.json)
-            if validate:
-                return make_response(jsonify(validate), 400)
-            else:
-                try:
-                    id_beer = id
-                    style_beer = request.json["style_beer"]
-                    id_best_beer_temperature = request.json["id_best_beer_temperature"]
-                    check_id_beer_temp = beer_repository.list_beer_temperature_by_id(id_best_beer_temperature)
-                    if check_id_beer_temp is None:
-                        return make_response("The id_best_beer_temperature passed does not exist in the database", 404)
-                    else:
-                        beer.id_beer = id_beer
-                        beer.style_beer = style_beer
-                        beer.id_best_beer_temperature = id_best_beer_temperature
-                        result = beer_service.update_beer(beer)
-                        return make_response(bs.jsonify(result), 200)
-                except Exception as e:
-                    return make_response("Check if the data was passed correctly or a bad code: " + str(e), 500)
-
-    def delete(self,id):
-        beer = beer_repository.list_beer_temperature_by_id(id)
-        if beer is None:
-            return make_response(jsonify("Beer ID not found"), 404)
-        else:
-            result = beer_temperature_service.delete_beer_temperature(beer)
-            return make_response('', 204)
     
 
 
 
 api.add_resource(BeerList, '/beers')
 api.add_resource(BeerDetail, '/beers/<int:id>')
-api.add_resource(BeerTemperatureDetail, '/beers/temperature/<int:temperature>')
+api.add_resource(BeerTemperaturePlaylistList, '/beers/temperature/playlist')
 api.add_resource(BeerTemperatureList, '/beers/temperature')
+api.add_resource(BeerTemperatureDetail, '/beers/temperature/<int:id>')
